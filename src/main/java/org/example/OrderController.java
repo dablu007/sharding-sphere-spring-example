837e34237968242678530d1264880aab1f678e9f
DABLU KUMAR <dablu.kumar007@gmail.com>
2023-12-31 17:24:36 +0530

    WIP changes for local running

--- src/main/java/org/example/OrderController.java
+++ src/main/java/org/example/OrderController.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/orders")
 public class OrderController {
 
-    private final OrderService orderService;
 
-    public OrderController(OrderService orderService) {
-        this.orderService = orderService;
-    }
+    OrderService orderService;
+
 
     @PostMapping
     public ResponseEntity<Order> createOrder(@RequestBody Order order) {
--- src/main/java/org/example/OrderService.java
+++ src/main/java/org/example/OrderService.java
@@ -5,11 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderService {
 
-    private final OrderRepository orderRepository;
-
-    public OrderService(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-    }
+    OrderRepository orderRepository;
 
     public Order createOrder(Order order) {
         return orderRepository.save(order);
